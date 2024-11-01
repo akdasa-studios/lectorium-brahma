a6db6b62459071cbc60511e83086cf7e1bbd16e4
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2024-11-01 18:51:23 +0100

    fix: do not sync _design documents

--- src/shared/composables/useSync.ts
+++ src/shared/composables/useSync.ts
@@ -4,8 +4,18 @@ export function useSync() {
   const database = useDatabase()
 
   async function sync() {
-    await database.local.dictionary.replicateFrom(database.remote.dictionary)
-    await database.local.inboxTracks.sync(database.remote.inboxTracks)
+    await database.local.dictionary.replicateFrom(
+      database.remote.dictionary,
+      {
+        filter: (doc) => { return !doc._id.startsWith('_design/') }
+      },
+    )
+    await database.local.inboxTracks.sync(
+      database.remote.inboxTracks,
+      {
+        filter: (doc) => { return !doc._id.startsWith('_design/') }
+      },
+    )
   }
 
   return { sync }
